21e7d6695df3886804c7acca936a9dd72a9a33e3
maducodes <marih.silva@icloud.com>
2024-05-15 22:50:52 -0300

    Initial Commit

---
+++ Tests/RoutinerDSKitTests/RoutinerDSKitTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import RoutinerDSKit
+
+final class RoutinerDSKitTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
