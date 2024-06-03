484b95a4c7de5668107662b1bf73eba92c203110
maducodes <marih.silva@icloud.com>
2024-06-02 21:58:05 -0300

    [N/A] Fix: change variables to public

--- Sources/RoutinerDSKit/Classes/Colors.swift
+++ Sources/RoutinerDSKit/Classes/Colors.swift
@@ -4,110 +4,110 @@ import UIKit
 public struct Colors {
     public struct Primary {
         public struct Base {
-            static let background = UIColor(named: "primary-base-background")
-            static let black = UIColor(named: "primary--base-black")
-            static let white = UIColor(named: "primary-base-white")
+            public static let background = UIColor(named: "primary-base-background")
+            public static let black = UIColor(named: "primary--base-black")
+            public static let white = UIColor(named: "primary-base-white")
         }
         public struct Black {
-            static let black10 = UIColor(named: "primary-black-black10")
-            static let black20 = UIColor(named: "primary-black-black20")
-            static let black40 = UIColor(named: "primary-black-black40")
-            static let black60 = UIColor(named: "primary-black-black60")
-            static let black80 = UIColor(named: "primary-black-black80")
-            static let black100 = UIColor(named: "primary-black-black100")
+            public static let black10 = UIColor(named: "primary-black-black10")
+            public static let black20 = UIColor(named: "primary-black-black20")
+            public static let black40 = UIColor(named: "primary-black-black40")
+            public static let black60 = UIColor(named: "primary-black-black60")
+            public static let black80 = UIColor(named: "primary-black-black80")
+            public static let black100 = UIColor(named: "primary-black-black100")
         }
         public struct Blue {
-            static let blue10 = UIColor(named: "primary-blue-blue10")
-            static let blue20 = UIColor(named: "primary-blue-blue20")
-            static let blue40 = UIColor(named: "primary-blue-blue40")
-            static let blue60 = UIColor(named: "primary-blue-blue60")
-            static let blue80 = UIColor(named: "primary-blue-blue80")
-            static let blue100 = UIColor(named: "primary-blue-blue100")
+            public static let blue10 = UIColor(named: "primary-blue-blue10")
+            public static let blue20 = UIColor(named: "primary-blue-blue20")
+            public static let blue40 = UIColor(named: "primary-blue-blue40")
+            public static let blue60 = UIColor(named: "primary-blue-blue60")
+            public static let blue80 = UIColor(named: "primary-blue-blue80")
+            public static let blue100 = UIColor(named: "primary-blue-blue100")
         }
         public struct BlueInfo {
-            static let blue10 = UIColor(named: "primary-blue-info-blue10")
-            static let blue20 = UIColor(named: "primary-blue-info-blue20")
-            static let blue40 = UIColor(named: "primary-blue-info-blue40")
-            static let blue60 = UIColor(named: "primary-blue-info-blue60")
-            static let blue80 = UIColor(named: "primary-blue-info-blue80")
-            static let blue100 = UIColor(named: "primary-blue-info-blue100")
+            public static let blue10 = UIColor(named: "primary-blue-info-blue10")
+            public static let blue20 = UIColor(named: "primary-blue-info-blue20")
+            public static let blue40 = UIColor(named: "primary-blue-info-blue40")
+            public static let blue60 = UIColor(named: "primary-blue-info-blue60")
+            public static let blue80 = UIColor(named: "primary-blue-info-blue80")
+            public static let blue100 = UIColor(named: "primary-blue-info-blue100")
         }
         public struct GreenSuccess {
-            static let green10 = UIColor(named: "primary-green-success-green10")
-            static let green20 = UIColor(named: "primary-green-success-green20")
-            static let green40 = UIColor(named: "primary-green-success-green40")
-            static let green60 = UIColor(named: "primary-green-success-green60")
-            static let green80 = UIColor(named: "primary-green-success-green80")
-            static let green100 = UIColor(named: "primary-green-success-green100")
-
+            public static let green10 = UIColor(named: "primary-green-success-green10")
+            public static let green20 = UIColor(named: "primary-green-success-green20")
+            public static let green40 = UIColor(named: "primary-green-success-green40")
+            public static let green60 = UIColor(named: "primary-green-success-green60")
+            public static let green80 = UIColor(named: "primary-green-success-green80")
+            public static let green100 = UIColor(named: "primary-green-success-green100")
+            
         }
         public struct OrangeWarning {
-            static let orange10 = UIColor(named: "primary-orange-warning-orange10")
-            static let orange20 = UIColor(named: "primary-orange-warning-orange20")
-            static let orange40 = UIColor(named: "primary-orange-warning-orange40")
-            static let orange60 = UIColor(named: "primary-orange-warning-orange60")
-            static let orange80 = UIColor(named: "primary-orange-warning-orange80")
-            static let orange100 = UIColor(named: "primary-orange-warning-orange100")
-
+            public static let orange10 = UIColor(named: "primary-orange-warning-orange10")
+            public static let orange20 = UIColor(named: "primary-orange-warning-orange20")
+            public static let orange40 = UIColor(named: "primary-orange-warning-orange40")
+            public static let orange60 = UIColor(named: "primary-orange-warning-orange60")
+            public static let orange80 = UIColor(named: "primary-orange-warning-orange80")
+            public static let orange100 = UIColor(named: "primary-orange-warning-orange100")
+            
         }
         public struct RedError {
-            static let red10 = UIColor(named: "primary-red-error-red10")
-            static let red20 = UIColor(named: "primary-red-error-red20")
-            static let red40 = UIColor(named: "primary-red-error-red40")
-            static let red60 = UIColor(named: "primary-red-error-red60")
-            static let red80 = UIColor(named: "primary-red-error-red80")
-            static let red100 = UIColor(named: "primary-red-error-red100")
+            public static let red10 = UIColor(named: "primary-red-error-red10")
+            public static let red20 = UIColor(named: "primary-red-error-red20")
+            public static let red40 = UIColor(named: "primary-red-error-red40")
+            public static let red60 = UIColor(named: "primary-red-error-red60")
+            public static let red80 = UIColor(named: "primary-red-error-red80")
+            public static let red100 = UIColor(named: "primary-red-error-red100")
         }
     }
     
     public struct Secondary {
         public struct DarkBlue {
-            static let darkBlue10 = UIColor(named: "secondary-darkblue-darkblue10")
-            static let darkBlue20 = UIColor(named: "secondary-darkblue-darkblue20")
-            static let darkBlue40 = UIColor(named: "secondary-darkblue-darkblue40")
-            static let darkBlue60 = UIColor(named: "secondary-darkblue-darkblue60")
-            static let darkBlue80 = UIColor(named: "secondary-darkblue-darkblue80")
-            static let darkBlue100 = UIColor(named: "secondary-darkblue-darkblue100")
+            public static let darkBlue10 = UIColor(named: "secondary-darkblue-darkblue10")
+            public static let darkBlue20 = UIColor(named: "secondary-darkblue-darkblue20")
+            public static let darkBlue40 = UIColor(named: "secondary-darkblue-darkblue40")
+            public static let darkBlue60 = UIColor(named: "secondary-darkblue-darkblue60")
+            public static let darkBlue80 = UIColor(named: "secondary-darkblue-darkblue80")
+            public static let darkBlue100 = UIColor(named: "secondary-darkblue-darkblue100")
         }
         public struct Orange {
-            static let orange10 = UIColor(named: "secondary-orange-orange10")
-            static let orange20 = UIColor(named: "secondary-orange-orange20")
-            static let orange40 = UIColor(named: "secondary-orange-orange40")
-            static let orange60 = UIColor(named: "secondary-orange-orange60")
-            static let orange80 = UIColor(named: "secondary-orange-orange80")
-            static let orange100 = UIColor(named: "secondary-orange-orange100")
+            public static let orange10 = UIColor(named: "secondary-orange-orange10")
+            public static let orange20 = UIColor(named: "secondary-orange-orange20")
+            public static let orange40 = UIColor(named: "secondary-orange-orange40")
+            public static let orange60 = UIColor(named: "secondary-orange-orange60")
+            public static let orange80 = UIColor(named: "secondary-orange-orange80")
+            public static let orange100 = UIColor(named: "secondary-orange-orange100")
         }
         public struct Pink {
-            static let pink10 = UIColor(named: "secondary-pink-pink10")
-            static let pink20 = UIColor(named: "secondary-pink-pink20")
-            static let pink40 = UIColor(named: "secondary-pink-pink40")
-            static let pink60 = UIColor(named: "secondary-pink-pink60")
-            static let pink80 = UIColor(named: "secondary-pink-pink80")
-            static let pink100 = UIColor(named: "secondary-pink-pink100")
+            public static let pink10 = UIColor(named: "secondary-pink-pink10")
+            public static let pink20 = UIColor(named: "secondary-pink-pink20")
+            public static let pink40 = UIColor(named: "secondary-pink-pink40")
+            public static let pink60 = UIColor(named: "secondary-pink-pink60")
+            public static let pink80 = UIColor(named: "secondary-pink-pink80")
+            public static let pink100 = UIColor(named: "secondary-pink-pink100")
         }
         public struct Purple {
-            static let purple10 = UIColor(named: "secondary-purple-purple10")
-            static let purple20 = UIColor(named: "secondary-purple-purple20")
-            static let purple40 = UIColor(named: "secondary-purple-purple40")
-            static let purple60 = UIColor(named: "secondary-purple-purple60")
-            static let purple80 = UIColor(named: "secondary-purple-purple80")
-            static let purple100 = UIColor(named: "secondary-purple-purple100")
+            public static let purple10 = UIColor(named: "secondary-purple-purple10")
+            public static let purple20 = UIColor(named: "secondary-purple-purple20")
+            public static let purple40 = UIColor(named: "secondary-purple-purple40")
+            public static let purple60 = UIColor(named: "secondary-purple-purple60")
+            public static let purple80 = UIColor(named: "secondary-purple-purple80")
+            public static let purple100 = UIColor(named: "secondary-purple-purple100")
         }
         public struct Teal {
-            static let teal10 = UIColor(named: "secondary-teal-teal10")
-            static let teal20 = UIColor(named: "secondary-teal-teal20")
-            static let teal40 = UIColor(named: "secondary-teal-teal40")
-            static let teal60 = UIColor(named: "secondary-teal-teal60")
-            static let teal80 = UIColor(named: "secondary-teal-teal80")
-            static let teal100 = UIColor(named: "secondary-teal-teal100")
+            public static let teal10 = UIColor(named: "secondary-teal-teal10")
+            public static let teal20 = UIColor(named: "secondary-teal-teal20")
+            public static let teal40 = UIColor(named: "secondary-teal-teal40")
+            public static let teal60 = UIColor(named: "secondary-teal-teal60")
+            public static let teal80 = UIColor(named: "secondary-teal-teal80")
+            public static let teal100 = UIColor(named: "secondary-teal-teal100")
         }
         public struct Yellow {
-            static let yellow10 = UIColor(named: "secondary-yellow-yellow10")
-            static let yellow20 = UIColor(named: "secondary-yellow-yellow20")
-            static let yellow40 = UIColor(named: "secondary-yellow-yellow40")
-            static let yellow60 = UIColor(named: "secondary-yellow-yellow60")
-            static let yellow80 = UIColor(named: "secondary-yellow-yellow80")
-            static let yellow100 = UIColor(named: "secondary-yellow-yellow100")
+            public static let yellow10 = UIColor(named: "secondary-yellow-yellow10")
+            public static let yellow20 = UIColor(named: "secondary-yellow-yellow20")
+            public static let yellow40 = UIColor(named: "secondary-yellow-yellow40")
+            public static let yellow60 = UIColor(named: "secondary-yellow-yellow60")
+            public static let yellow80 = UIColor(named: "secondary-yellow-yellow80")
+            public static let yellow100 = UIColor(named: "secondary-yellow-yellow100")
         }
     }
     
